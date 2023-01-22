0203923b19c16fd44540bb51352e7a958403864e
Drummer Dee <drummer.deja@gmail.com>
2023-01-21 19:28:30 -0500

    attempt to connect sql database

---
+++ we-care/src/Components/Help.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Help = () => {
+  return (
+    <div>
+        <h3> Need Assistance </h3>
+        <span> Call us at 1-800-932-2730 </span>
+    </div>
+  )
+}
+export default Help;
