dbabc7a8300d4f7f7de0c80fff124191127a64d9
Drummer Dee <drummer.deja@gmail.com>
2023-05-20 15:14:09 -0400

    Login skeleton

--- we-care/src/Pages/LoginMem.js
+++ we-care/src/Pages/LoginMem.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Grid,Container, Paper} from "@mui/material"
+import {Grid,Container, Paper, TextField, Button} from "@mui/material"
 
 const LoginMem = () => {
   console.log('Login member page');
@@ -11,16 +11,22 @@ const LoginMem = () => {
         justifyContent='center'
         style = {{minHeight:'100vh'}}
         >
-  <Paper elevation={3} sx={{padding:5}}>
+  <Paper elevation={3} sx={{padding:5}} style ={{backgroundColor:'green'}}>
   <Grid container 
         spacing={2} 
         direction = 'column' 
         >
-          <Grid item xs={8}>
+          <Grid item xs={8} spacing ={6}>
+            <TextField fullWidth id='standard-basic' label='email' variant='standard'/>
   </Grid>
   <Grid item xs={4}>
+<TextField id = 'standard-basic' label = 'password' variant = 'standard'/>
   </Grid>
   <Grid item xs={4}>
+    <Button 
+      onClick = {() => alert('I have been clicked')} 
+      variant='contained' 
+      justifyContent='center'> Login </Button>
   </Grid>
   <Grid item xs={8}>
   </Grid>
